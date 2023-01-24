5d681379cf26eff6da0c5d0b4a10e40c2e6ca8c1
JoshuaHerring <jdherring2222@gmail.com>
2023-01-23 17:35:26 -0700

    cleaned old code from index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
 const routes = require("express").Router();
 
-const contacts = require("../controllers/contacts.js")
-
-
-// routes.use("/contacts", contacts.data);
 routes.use("/contacts", require("./contacts"));
 
 
