6373f53a3042b47854ef455f73631a06c209ec02
JoshuaHerring <jdherring2222@gmail.com>
2023-05-09 15:57:02 -0600

    added delete

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 const port = process.env.port || 8080;
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT")
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE")
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     next();
   })
